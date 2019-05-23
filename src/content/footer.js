e48ec31bbd5326b14fc37095dbb7ab04e6b8437d
= <=>
2019-05-23 00:04:09 -0300

    adjust layout and links

--- src/content/footer.js
+++ src/content/footer.js
@@ -3,23 +3,23 @@ module.exports = {
         {
             title: 'Parceiros',
             items: [
-                { text: 'Local', link: '#section-main' },
+                { text: 'Local', link: 'https://goo.gl/maps/m7ttyu5CCnBjobmD7' },
                 { text: 'Keynote', link: '#section-keynote' },
                 { text: 'Evento', link: '#section-about' },
-                { text: 'Programação', link: '#' },
-                { text: 'Código de Contuda', link: '#' },
+                // { text: 'Programação', link: '#' },
+                { text: 'Código de Conduda', link: 'https://python.org.br/cdc/' },
             ]
         },
         {
             title: 'Patrocínio',
             items: [
-                { text: 'Quero Patrocinar', link: '#' },
+                { text: 'Quero Patrocinar', link: 'https://github.com/pythonbrasil/pythonbrasil-2019-site/raw/master/assets/pdfs/plano_de_patrocinio.pdf' },
                 { text: 'Patrocinadores', link: '#section-sponser' },
             ]
         },
     ],
     social: {
-        facebook: { link: 'https://www.facebook.com/groups/python.brasil', logo: '/images/facebook.svg' },
+        facebook: { link: 'https://www.facebook.com/pythonbrasil/', logo: '/images/facebook.svg' },
         twitter: { link: 'https://twitter.com/pythonbrasil', logo: '/images/twitter.svg' },
         instagram: { link: 'https://www.instagram.com/pythonbrasil/', logo: '/images/instagram.svg' },
         apybr: { link: 'https://python.org.br/', logo: '/images/pylogo.svg' }
