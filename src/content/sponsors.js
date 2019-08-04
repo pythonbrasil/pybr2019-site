11d54979fb1e26151e63d5563d84885402bcbb41
Fabio Utzig <utzig@apache.org>
2019-08-04 10:53:48 -0300

    Add missing information
    - Add PSF as platinum
    - Publish keynote: Lais Varejao

--- src/content/sponsors.js
+++ src/content/sponsors.js
@@ -8,18 +8,24 @@ module.exports = {
         title: 'Patrocinadores',
         categories: [
             {
+                type: 'platina',
+                items: [
+                    Entity('PSF', 'psf-logo-narrow-256x84-alpha.png', 'https://www.python.org/psf'),
+                ],
+            },
+            {
                 type: 'ouro',
                 items: [
                     Entity('Loadsmart', 'loadsmart-logo-horizontal.png', 'https://www.loadsmart.com'),
                     Entity('olist', 'olist.png', 'https://olist.com'),
-                    Entity('Stoodi', 'stoodi.png', 'https://www.stoodi.com.br/')
+                    Entity('Stoodi', 'stoodi.png', 'https://www.stoodi.com.br/'),
                 ],
             },
             {
                 type: 'prata',
                 items: [
                     Entity('Evolux', 'logo-evolux.svg', 'https://www.evolux.net.br'),
-                    Entity('Lab Codes', 'labcodes.png', 'https://labcodes.com.br/')
+                    Entity('Lab Codes', 'labcodes.png', 'https://labcodes.com.br/'),
                 ],
             }
         ]
@@ -29,7 +35,6 @@ module.exports = {
         title: 'Parceiros',
         items: [
           Entity('associação python brasil', 'apyb.png', 'https://python.org.br/'),
-
         ]
       }
     ],
